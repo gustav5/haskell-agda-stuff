4442ee6229e19b58d6c59df712c88171ccfc9f54
Gustav Sahlin <62982253+gustav5@users.noreply.github.com>
2022-02-09 11:07:29 +0100

    half of y, not s

--- karatsuba_mul.hs
+++ karatsuba_mul.hs
@@ -9,11 +9,9 @@ str_tup_to_int (x,y) = (read x :: Int, read y :: Int)
 split_num :: Int -> (Int,Int)
 split_num x = str_tup_to_int (int_to_tup x) 
 
+karatsuba :: Integer -> Integer -> Integer
+karatsuba x y | x < 10 || y < 10 = x*y 
+              | otherwise =  let (r,s) = ((split_num x),(split_num y)) in karatsuba' (fst r) (snd r) (fst s) (snd s) (div ((toInteger . length . show) y) 2)
 
-
-karatsuba :: Int -> Int -> Int
-karatsuba x y | ((length  (show x)) < 10) || ((length (show y)) < 10) = x*y
-              | otherwise =  let (r,s) = ((split_num x),(split_num y)) in karatsuba' (fst r) (snd r) (fst s) (snd s) (max (length r) (length s))
-
-karatsuba' :: Int -> Int -> Int -> Int -> Int -> Int
-karatsuba' a b c d n = let (ac,bd) = (karatsuba a c, karatsuba b d) in (10^n)*ac + ((10^(div n 2))*(karatsuba (a+b) (c+d)) - ac - bd )+ bd
+karatsuba' :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+karatsuba' a b c d half = let (ac,bd) = (karatsuba a c, karatsuba b d) in (10^(2*half))*ac + (10^half)*((karatsuba (a+b) (c+d)) - ac - bd ) + bd
