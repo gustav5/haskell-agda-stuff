15e66213bee2b85ce64c646455812615cebbfbe3
Gustav Sahlin <62982253+gustav5@users.noreply.github.com>
2022-04-19 12:58:53 +0200

    Update karatsuba_poly.hs

--- karatsuba_poly.hs
+++ karatsuba_poly.hs
@@ -1,6 +1,33 @@
 
 import Test.QuickCheck
 
+addPoly :: [Int] -> [Int] -> [Int]
+addPoly [] ys = ys
+addPoly xs [] = xs
+addPoly (x:xs) (y:ys) = x + y : addPoly xs ys 
+
+
+
+subPoly :: [Int] -> [Int] -> [Int]
+subPoly xs ys = addPoly xs (negPoly ys)
+
+negPoly :: [Int] -> [Int]
+negPoly [] = []
+negPoly (x:xs) = (-x) : negPoly xs  
+
+-- shift_right 2 [1,2] will produce [0,0,1,2]
+shift_right :: Int -> [Int] -> [Int]
+shift_right n xs = (replicate n 0) ++ xs
+
+shiftRight' :: Int -> [Int] -> [Int]
+shiftRight' 0 xs = xs
+shiftRight' n xs = 0 :: shiftRight (n-1) xs
+
+mulPoly :: [Int] -> [Int] -> [Int]
+mulPoly [] ys = []
+mulPoly xs [] = []
+mulPoly (x:xs) ys = addPoly (map (x*) ys) (0:mulPoly xs ys)
+
 
 -----------------------------------------
 ------------------- Karatsuba
@@ -22,28 +49,6 @@ karatsuba xs ys  =   let m = min (div (length xs) 2) (div (length ys) 2) in
                       let ad_plus_bc = ((karatsuba a_plus_b c_plus_d) `subPoly` ac) `subPoly` bd   in
                       ((shift_right (2*m) ac) `addPoly` (shift_right m ad_plus_bc)) `addPoly`  bd
 
-addPoly :: [Int] -> [Int] -> [Int]
-addPoly [] ys = ys
-addPoly xs [] = xs
-addPoly (x:xs) (y:ys) = x + y : addPoly xs ys 
-
-
-
-subPoly :: [Int] -> [Int] -> [Int]
-subPoly xs ys = addPoly xs (negPoly ys)
-
-negPoly :: [Int] -> [Int]
-negPoly [] = []
-negPoly (x:xs) = (-x) : negPoly xs  
-
--- shift_right 2 [1,2] will produce [0,0,1,2]
-shift_right :: Int -> [Int] -> [Int]
-shift_right n xs = (replicate n 0) ++ xs
-
-mulPoly :: [Int] -> [Int] -> [Int]
-mulPoly [] ys = []
-mulPoly xs [] = []
-mulPoly (x:xs) ys = addPoly (map (x*) ys) (0:mulPoly xs ys)
 
 -----------------------------------------
 ------------------- Test
